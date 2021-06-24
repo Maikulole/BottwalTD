c1eb0ef388e217107da4615f6bef63386b2da9d4
Maikulole <t.fleischklops@gmail.com>
2021-06-24 21:39:35 +0200

    adding main menu scripts

---
+++ src/View/MainMenuView.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class MainMenuView : Button
+{
+    private MainViewModel viewModel;
+
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        viewModel = new MainViewModel(); //TODO: Gibt es ein schönes Pattern für View/Viewmodel in Godot? Kp man
+    }
+
+    private void OnQuitButtonUp()
+    {
+    }
+}
