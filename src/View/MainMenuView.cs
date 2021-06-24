ef5844355e02770c1e358b83b93b0f22b67e9c58
Maikulole <t.fleischklops@gmail.com>
2021-06-25 01:08:39 +0200

    adding quit button functionality

--- src/View/MainMenuView.cs
+++ src/View/MainMenuView.cs
@@ -3,15 +3,16 @@ using System;
 
 public class MainMenuView : Button
 {
-    private MainViewModel viewModel;
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        viewModel = new MainViewModel(); //TODO: Gibt es ein schönes Pattern für View/Viewmodel in Godot? Kp man
     }
-    private void OnButtonPressed(string start_scene)
+    private void OnButtonPressed(string sceneToNavigate)
     {
-        viewModel.NavigateToScene(start_scene);
+        switch(sceneToNavigate)
+        {
+            case "quit_game": GetTree().Quit(); break;
+            default: break;
+        }
     }
 }
